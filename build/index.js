bc14e80983bf805163dd80908f1e05b6a5ae73fe
GRI <gri-camp@yandex.ru>
2024-01-05 13:34:53 +0300

    RefactorInit

--- build/index.js
+++ build/index.js
@@ -1,5 +1,2 @@
 import Calendar from "./services/Calendar.js";
 const calendar = new Calendar({ time: true });
-calendar.addSelectorStyles("$calendar", "font-weight: 700;");
-calendar.addSelectorStyles("$calendar", "font-style: italic;");
-calendar.removeSelectorStyles("$calendar");
--- index.ts
+++ index.ts
@@ -3,10 +3,3 @@ import Calendar from "./services/Calendar.js";
 
 const calendar = new Calendar({time: true});
 
-// API
-
-calendar.addSelectorStyles("$calendar", "font-weight: 700;")
-
-calendar.addSelectorStyles("$calendar", "font-style: italic;")
-
-calendar.removeSelectorStyles("$calendar")
